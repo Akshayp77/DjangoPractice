7f884ff05dadad82830f1ee048fa6a5462bc66b5
Akshay <87304387+Akshayp77@users.noreply.github.com>
2021-07-21 19:00:42 +0530

    Update view.py

--- calc/views.py
+++ calc/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 def Index(request):
     return HttpResponse("Hello World")
+@csrf_exempt
 def add(request):
     if request.method == 'GET':
         val1=request.GET['num1']
         val2=request.GET['num2']
 
         return HttpResponse(int(val1)+int(val2))
+    elif request.method == 'POST':
+        
+        print("+"*20)
+        print(request)
+        print("+"*20)
+        val1=request.POST['num1']
+        val2=request.POST['num2']
+
+        return HttpResponse(int(val1)+int(val2))
     else:
-        return HttpResponse("No response")
+        return HttpResponse()
